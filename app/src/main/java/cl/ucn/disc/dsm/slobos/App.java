1de5deee5cb9a6057f47ed435be45eec2cecc35c
sebastlobos <sebastian.lobos@alumnos.ucn.cl>
2021-11-07 02:25:31 -0300

    Add text in spanish and email config

--- app/src/main/java/cl/ucn/disc/dsm/slobos/App.java
+++ app/src/main/java/cl/ucn/disc/dsm/slobos/App.java
@@ -6,6 +6,10 @@ import android.content.Context;
 import org.acra.ACRA;
 import org.acra.BuildConfig;
 import org.acra.annotation.AcraCore;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.config.DialogConfigurationBuilder;
+import org.acra.config.MailSenderConfigurationBuilder;
+import org.acra.data.StringFormat;
 
 /**
  * Main App
@@ -21,7 +25,29 @@ public class App extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
+
+        //ACRA configuration
+        CoreConfigurationBuilder builder = new CoreConfigurationBuilder(this);
+        builder
+                .withBuildConfigClass(BuildConfig.class)
+                .withReportFormat(StringFormat.JSON)
+                .withEnabled(true);
+
+        //ACRA Dialog configuration
+        builder.getPluginConfigurationBuilder(DialogConfigurationBuilder.class)
+                .withResText(R.string.acra_dialog_title)
+                .withResCommentPrompt(R.string.acra_dialog_comment)
+                .withEnabled(true);
+
+        //ACRA email configuration
+        builder.getPluginConfigurationBuilder(MailSenderConfigurationBuilder.class)
+                .withMailTo("sebastian.lobos@alumnos.ucn.cl")
+                .withReportFileName("crash.txt")
+                .withSubject(getString(R.string.acra_dialog_title))
+                .withBody(getString(R.string.acra_dialog_comment))
+                .withEnabled(true);
+
         // The following line triggers the initialization of ACRA
-        ACRA.init(this);
+        ACRA.init(this, builder);
     }
 }
--- app/src/main/java/cl/ucn/disc/dsm/slobos/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/slobos/MainActivity.java
@@ -3,12 +3,21 @@ package cl.ucn.disc.dsm.slobos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**
+ * @author Sebastian Lobos Aravena
+ */
 public class MainActivity extends AppCompatActivity {
+    /**
+     *
+     * @param savedInstanceState context to use.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        throw new IllegalArgumentException("Error en la aplicacion!!");
     }
+
+
 }
